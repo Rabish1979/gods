6ad5efc50022c6673fd1ef47beb47645a461cf2e
Rabish Kumar <krabish@vmware.com>
2024-07-25 21:51:09 +0530

    fixes.

--- dstest/dstest.go
+++ dstest/dstest.go
@@ -2,14 +2,9 @@ package main
 
 import (
 	"fmt"
-    "github.com/Rabish1979/gods/linklist"
 )
 
 func main() {
-	myPlaylist := createLinkList()
-	myPlaylist.insertAtFront(20)
-	myPlaylist.insertAtFront(30)
-	myPlaylist.insertAtFront(40)
-	myPlaylist.Print()
+
 	fmt.Println("hello package")
 }
